4d44549958e9f478174c1a2f42310f5103676d18
mitrakakavand <mitrakakavand79@gmail.com>
2025-11-02 13:42:39 +0330

    server

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ async function loadHeader() {
   const data = await response.text();
   const header = document.getElementById("header");
   header.innerHTML = data;
-//dropdown menu
+
+  // --- dropdown menu ---
   const toggle = header.querySelector('.dropdown-toggle');
   const menu = header.querySelector('.dropdown-menu');
 
@@ -13,11 +14,31 @@ async function loadHeader() {
       menu.style.display = menu.style.display === 'none' || menu.style.display === '' ? 'flex' : 'none';
     });
 
-  
     document.addEventListener('click', (e) => {
       if (!menu.contains(e.target) && !toggle.contains(e.target)) {
         menu.style.display = 'none';
       }
+    });
+  }
+
+  // --- Login / Logout ---
+  const authButtons = header.querySelector("#auth-buttons");
+  const userBox = header.querySelector("#user-box");
+  const loginBtn = header.querySelector("#login-btn");
+  const logoutBtn = header.querySelector("#logout-btn");
+
+  if(loginBtn && logoutBtn && authButtons && userBox){
+    userBox.style.display = "none";
+    authButtons.style.display = "flex";
+
+    loginBtn.addEventListener("click", () => {
+      authButtons.style.display = "none"; 
+      userBox.style.display = "flex";     
+    });
+
+    logoutBtn.addEventListener("click", () => {
+      userBox.style.display = "none";     
+      authButtons.style.display = "flex"; 
     });
   }
 }
